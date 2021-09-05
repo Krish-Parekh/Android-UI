1111d1ddca820d7f6a6fe987a27070bc8d044a70
Krish Parekh <Krishp4123@gmail.com>
2021-09-05 19:04:51 +0530

    Bottom Sheet Comment added

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -18,11 +18,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /*
+        * Return the FragmentManager for interacting with fragments associated with this activity.
+        * */
         val navHostFragment =  supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        /*
+        * NavController manages app navigation within a NavHost.
+        * Apps will generally obtain a controller directly from a host, or by using one of the utility methods on
+        * the Navigation class rather than create a controller directly.
+        *
+        * Navigation flows and destinations are determined by the navigation graph owned by the controller.
+        * These graphs are typically inflated from an Android resource, but, like views, they can also be constructed
+        * or combined programmatically or for the case of dynamic navigation structure.
+        * */
+
         val navController = navHostFragment.navController
+        /*
+        * Sets up the ActionBar returned by AppCompatActivity.getSupportActionBar for use with a NavController.
+        * By calling this method, the title in the action bar will automatically be updated when the destination
+        * changes (assuming there is a valid label).
+        * */
         setupActionBarWithNavController(navController)
     }
-
+    /*
+    * onSupportNavigateUp :
+    * Handles the Up button by delegating its behavior to the given NavController. This should generally be called from
+    * */
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }

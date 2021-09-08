87e1d8864de2e9d8dae269d574d38f4f9c2a325e
Krish Parekh <Krishp4123@gmail.com>
2021-09-08 22:02:45 +0530

    Time Picker

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.uipractice
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.MediaController
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.text.format.DateFormat.is24HourFormat
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.uipractice.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,34 +21,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
-        * Return the FragmentManager for interacting with fragments associated with this activity.
-        * */
-        val navHostFragment =  supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
 
-        /*
-        * NavController manages app navigation within a NavHost.
-        * Apps will generally obtain a controller directly from a host, or by using one of the utility methods on
-        * the Navigation class rather than create a controller directly.
-        *
-        * Navigation flows and destinations are determined by the navigation graph owned by the controller.
-        * These graphs are typically inflated from an Android resource, but, like views, they can also be constructed
-        * or combined programmatically or for the case of dynamic navigation structure.
-        * */
+    }
 
-        val navController = navHostFragment.navController
-        /*
-        * Sets up the ActionBar returned by AppCompatActivity.getSupportActionBar for use with a NavController.
-        * By calling this method, the title in the action bar will automatically be updated when the destination
-        * changes (assuming there is a valid label).
-        * */
-        setupActionBarWithNavController(navController)
-    }
-    /*
-    * onSupportNavigateUp :
-    * Handles the Up button by delegating its behavior to the given NavController. This should generally be called from
-    * */
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+
+
+
+
 }
--- app/src/main/java/com/example/uipractice/homeFragment.kt
+++ app/src/main/java/com/example/uipractice/homeFragment.kt
@@ -1,16 +1,21 @@
 package com.example.uipractice
 
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.DatePicker
+import android.widget.Toast
 import com.example.uipractice.databinding.FragmentHomeBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 
 class homeFragment : Fragment() {
-
     private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,10 +23,28 @@ class homeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater,container,false)
-        binding.button.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment2_to_bottomSheetFragment3)
+        binding.btnCal.setOnClickListener {
+            openTimePicker()
+        }
+        return binding.root
+    }
+    private fun openTimePicker() {
+        val is24System = DateFormat.is24HourFormat(requireContext())
+        val clockFormat = if(is24System) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+        val picker = MaterialTimePicker.Builder()
+            .setTimeFormat(clockFormat)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Set Alarm")
+            .build()
+        picker.show(childFragmentManager,"TAG")
+
+        picker.addOnPositiveButtonClickListener {
+            val h = picker.hour
+            val min = picker.minute
+            binding.tvCal.text = "Time - $h:$min"
+            Log.d("Krish","$h $min")
         }
 
-        return binding.root
     }
 }
